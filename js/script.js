a6014fd4f10c9c22a0e52632ae60a9c39f71f255
Adam-Batiuk <159185155+Adam-Batiuk@users.noreply.github.com>
2024-04-14 19:45:30 +0000

    done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  //input
+  const radius = parseInt(document.getElementById("radius").value)
+  //proses
+  const volume = 4/3 * Math.PI * radius ** 3
+  const roundVloume = Math.round(volume * 100) / 100
+  //output
+  document.getElementById("answer").innerHTML = "Volume is: " + roundVloume + "mm³"
+}
